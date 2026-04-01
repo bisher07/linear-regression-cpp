2d6c6c51f86838b3c1585385c637c0310c31057f
Bisher <bisher.madi84@gmail.com>
2026-04-02 02:12:00 +0400

    Add train function with gradient descent

--- src/regression.cpp
+++ src/regression.cpp
@@ -1,8 +1,8 @@
-
 #include "regression.h"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 
 void DataSet::load(const std::string &filename)
 {
@@ -19,9 +19,45 @@ void DataSet::load(const std::string &filename)
         std::string hoursStr = line.substr(0, commaIndex);
         std::string gradesStr = line.substr(commaIndex + 1);
 
-        point.hours = std::stof(hoursStr);
-        point.grade = std::stof(gradesStr);
+        point.hours = std::stod(hoursStr);
+        point.grade = std::stod(gradesStr);
 
         points.push_back(point);
     }
+}
+
+void LinearRegression::train(const DataSet &data)
+{
+    double gradient_m, gradient_b, loss, predicted;
+
+    for (int i = 0; i < 1000; i++)
+    {
+        loss = 0;
+        gradient_m = 0;
+        gradient_b = 0;
+
+        for (int point = 0; point < data.size(); point++)
+        {
+            predicted = m * data.getPoint(point).hours + b;
+            gradient_m += (predicted - data.getPoint(point).grade) * data.getPoint(point).hours;
+            gradient_b += (predicted - data.getPoint(point).grade);
+            loss += pow((predicted - data.getPoint(point).grade), 2);
+        }
+
+        gradient_m /= data.size();
+        gradient_b /= data.size();
+        loss /= data.size();
+
+        m -= (learningRate * gradient_m);
+        b -= (learningRate * gradient_b);
+
+        if (i % 100 == 0)
+        {
+            std::cout << "Iteration " << i << ":\t" << "Loss: " << loss << std::endl;
+        }
+    }
+}
+
+double LinearRegression::predict(double x)
+{
 }
